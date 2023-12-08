9abd8fc9213727d1b878e744b5f047a55bb46efb
Soumyajit Sinha <153104669+Macbeth004@users.noreply.github.com>
2023-12-08 15:56:13 +0530

    Add files via upload

---
+++ 01_varieables.js
@@ -0,0 +1,5 @@
+const accountId = 12345
+let accountEmail ="macbeth@gmail.com"
+var accountPassword = "sinha@4323"
+accountCity= "Kolkata"
+console.table([accountId,accountPassword,accountCity,accountEmail])
---
+++ 02_datatype.js
@@ -0,0 +1,32 @@
+"use strict" /* treat all js code as newer verson 
+old js code a ato veriable chilo na akon new code
+ a onk variable asagach tai aaai comand ta diya 
+ dila brouser old code use na korea only new version code use kora strictly*/
+ 
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Soumyajit")
+
+
+let name = "Soumyajit"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //  ans-undefined
+console.log(typeof null); // ans-object
